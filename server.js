f89052a6b8e5b99e0819acaf32941e14e601bf4f
MIGUEL PAULINO ALVES PEREIRA <miguelpaulino2009.ap@gmail.com>
2026-09-15 15:51:02 -0300

    Salvando

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const conexao = require("./db.js");
 
 // import express from "express";
 // import cors from "cors";
@@ -26,9 +27,20 @@ app.get("/",(req, res)=>{
     })
 });
 
-app.get("/alunos",(req,res)=>{
-    res.json(ALUNOS);
-});
+app.get("/alunos", async (req,res)=>{
+    
+    
+    try {
+        const [resultado] = await conexao.query(`SELECT * FROM alunos;`)
+        res.status(200).json(resultado);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            mensagem:"Erro ao buscar alunos"
+        })
+    };
+    }
+);
 
 app.get("/alunos/:id", (req,res)=>{
     const id = Number(req.params.id);
